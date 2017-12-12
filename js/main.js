e8054c2bf6b6b8e92540245b42a3f8652396c78d
Ivana Petrovic <ivaptt@gmail.com>
2017-12-12 14:50:33 +0100

    sredjivanje countera- sekcija numbers.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-$(document).ready(function(){
-    
-     $('.navbar-nav a').click(function () {
+$(document).ready(function () {
+
+    $('.navbar-nav a, .landing .container > a').click(function () {
         $('html, body').animate({
             scrollTop: $($.attr(this, 'href')).offset().top
         }, 800);
     });
-    
-    
+
+
     $(window).scroll(function () {
         var scroll = $(window).scrollTop();
 
@@ -22,9 +22,46 @@ $(document).ready(function(){
 
     });
 
-    
-    
-    
-    
-    
+
+
+//ROUND SLIDER FOR PERCENT
+
+    $(window).scroll(function () {
+        percent();
+
+    });
+    percent();
+
+
+
+
+    function percent() {
+        var vh = $(window).height();
+        var scroll = $(window).scrollTop();
+
+        $('.cont').each(function () {
+            var position = $(this).offset().top;
+
+
+            if (position < vh + scroll - 100) {
+
+                var val = parseInt($(this).attr('data-pct'));
+                var $circle = $(this).find('.svg .bar');
+
+
+                var r = $circle.attr('r');
+                var c = Math.PI * (r * 2);
+
+
+                var pct = ((100 - val) / 100) * c;
+
+                $circle.css({strokeDashoffset: pct});
+            }
+
+
+        });
+    }
+
+
+
 });
